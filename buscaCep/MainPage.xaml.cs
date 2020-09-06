c114437697ac51793b5eabe0b3f9d15bc5d7d5c2
Leticia Malipense <leticia.malipense@gmail.com>
2020-09-05 23:53:20 -0300

    first commit

---
+++ buscaCep/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using buscaCep.Servico;
+using buscaCep.Servico.Modelo;
+
+namespace buscaCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            String auxiliar = CEP.Text.Trim();
+
+            Endereco end = ServicoViaCep.BuscarEndereco(auxiliar); 
+
+            Resultado.Text = String.Format("Endereço: {0}, de {1}, {2} - {3}",
+                                    end.logradouro, end.bairro, end.localidade, end.uf); 
+
+            CEP.Text = "";
+        }
+    }
+}
---
+++ buscaCep/Servico/ServicoViaCep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using buscaCep.Servico.Modelo;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace buscaCep.Servico
+{
+    public class ServicoViaCep
+    {
+        private static String url = "https://viacep.com.br/ws/{0}/json/";
+
+        public ServicoViaCep()
+        {
+        }
+
+        public static Endereco BuscarEndereco(String cep)
+        {
+            String novaUrl = String.Format(url, cep);
+
+            WebClient wc = new WebClient();
+
+            String conteudo = wc.DownloadString(novaUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
